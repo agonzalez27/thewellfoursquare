460ff640510acb27c7f35542b8e9b50e9e71be76
Andrew Gonzalez <andrew.gonzalez@gmail.com>
2017-01-10 12:11:44 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+(function() {
+  'use strict';
+
+  angular.module('theWell', ['ui.router']);
+
+}());
---
+++ js/config/routes.js
@@ -0,0 +1,32 @@
+
+(function() {
+  'use strict';
+
+  angular.module('theWell')
+    .config(MainRouter);
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function MainRouter($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+      .state('ministries', {
+        url: '/ministries',
+        templateUrl: 'js/templates/ministries.html'
+      })
+      .state('ourTeam', {
+        url: '/ourteam',
+        templateUrl: 'js/templates/ourTeam.html'
+      })
+      .state('media', {
+        url: '/media',
+        templateUrl: 'js/templates/media.html'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'js/templates/contact.html'
+      })
+  }
+}());
