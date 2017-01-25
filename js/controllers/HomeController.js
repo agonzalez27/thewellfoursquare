7258364477457223be389eefa33d2a7288a421bb
Andrew Gonzalez <andrew.gonzalez@gmail.com>
2017-01-25 09:25:19 -0800

    added prayer request form

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -4,15 +4,39 @@
     angular.module('theWell')
     .controller('HomeController', HomeController)
 
-    HomeController.$inject = ['$location', '$anchorScroll']
+    HomeController.$inject = ['$location', '$anchorScroll', '$http']
 
-    function HomeController ($location, $anchorScroll) {
+    function HomeController ($location, $anchorScroll, $http) {
     var vm = this;
 
       vm.scrollTo = function(scrollLocation) {
         $location.hash(scrollLocation);
         $anchorScroll();
       }
-    }
+
+      vm.sendMail = function () {
+        var data = ({
+            contactName : vm.contactName,
+            contactEmail : vm.contactEmail,
+            contactMsg : vm.contactMsg
+        });
+        console.log(data)
+        // Simple POST request example (passing data) :
+        $http.post('http://localhost:3000/api/prayerRequest', data).
+            success(function(data, status, headers, config) {
+                // vm callback will be called asynchronously
+                // when the response is available
+                Materialize.toast('Thanks for your message ' + data.contactName + "."  + "  Our prayer team will be in conctact with you shortly.", 4000);
+
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+
+      };
+  }
+
+
 
 }());
